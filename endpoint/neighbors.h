e511b0bb2b455b36befeac8380b3958fb2879dba
xyz <xyz@xyzs-MBA.local>
2013-10-27 18:21:25 +0800

    seperate the neighbors info

---
+++ endpoint/neighbors.h
@@ -0,0 +1,9 @@
+#ifndef NEIGHBORS
+#define NEIGHBORS
+
+char local_mac_addr[] = {0x00,0xff,0xec,0xbb,0x1d,0xe3};
+char local_ip_addr[] = "192.168.81.129";
+char gate_mac_addr[] = {0x00,0x0c,0x29,0xa1,0x6c,0xbc};
+char twohopnei_ip_addr[] = "192.168.81.132";
+
+#endif
